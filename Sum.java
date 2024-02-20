20fea8ae381642fbd0003b96d04fdad64be4f6bb
hritikmohan <hhritikmohan@gmail.com>
2024-02-20 14:57:27 +0530

    Initial Commit

---
+++ Sum.java
@@ -0,0 +1,10 @@
+class Sum
+{
+    public static void main(String a[])
+    {
+        int num1 =2;
+        int num2 =6;
+        int result = num1 + num2;
+        System.out.println(result);
+    }
+}
