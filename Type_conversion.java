b19d49a403117a6bf4eddc8af9e9e5272bfdefff
hritikmohan <hhritikmohan@gmail.com>
2024-02-21 11:07:12 +0530

    Type Conversion

---
+++ Type_conversion.java
@@ -0,0 +1,11 @@
+public class Type_conversion {
+    public static void main(String args[]){
+        // byte b = 127;
+        int n = 257;
+        byte k = (byte)n;
+        // System.out.println(b);
+        System.out.println(n);
+        System.out.println(k);
+    }
+    
+}
