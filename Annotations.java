23e349080cc1d5502ef4a9a1d5b452333f919503
hritikmohan <hhritikmohan@gmail.com>
2024-03-06 21:36:52 +0530

    Annotations

---
+++ Annotations.java
@@ -0,0 +1,19 @@
+// Annotations are tags that represent meta data to the compiler
+// They do not affect the functionality of the code
+// They provide additional information to the compiler and jvm
+// @ is used for annotations
+
+
+class A{
+    public void show(){}
+}
+
+class B{
+    @Override
+    public void show(){}
+}
+
+public class Annotations {
+
+    
+}
